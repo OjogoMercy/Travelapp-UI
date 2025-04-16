885fbd328d2e0527e63d2fdc0d0cc15cf76c3186
Armykookie35 <mercyojogo@gmail.com>
2025-04-15 22:23:38 -0700

    updates

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,30 @@
-import { SafeAreaView, StyleSheet, Text, View, StatusBar,Image,TextInput, TouchableOpacity } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, StatusBar,Image,TextInput, TouchableOpacity,FlatList } from 'react-native';
 import React from 'react';
 import images from '@/assets/images/Images';
 import { Ionicons } from '@expo/vector-icons';
+import Database from "../constants/Database";
 
 const HomeScreen = () => {
+    const renderItem = ({ item }) => (
+        <View style={styles.itemContainer}>
+          <Image source={item.image} style={styles.images} />
+          <Text style={styles.title}>{item.title}</Text>
+          <Text style={styles.description}>{item.description}</Text>
+        </View>
+      );
+
+      const renderItems = ({ item }) => (
+        <View style={{flexDirection:'row',justifyContent:'space-between',padding:5,alignItems:'center'}}>
+          <View style={styles.contain}>
+          <Image source={item.image} style={{height:60,width:60,borderRadius:20}}/>
+          <View style={{padding:5,width:'87%'}}>
+          <Text style={{fontSize:14,fontWeight:'bold',color:'gray'}}>{item.title}</Text>
+          <Text style={styles.description}>{item.description} </Text>
+          </View>
+          </View>
+         
+        </View>
+      );
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar backgroundColor={'#e4f0ff'} />
@@ -28,16 +49,35 @@ const HomeScreen = () => {
          </TouchableOpacity>
       </View>
       <View style={styles.tab}>
-        <View style={[styles.con, {backgroundColor:'#78bdfc'}]}>
+        <TouchableOpacity style={[styles.con, {backgroundColor:'#70c2fb'}]}>
             <Text style={styles.tabtext}>Sights</Text>
-        </View>
-         <View style={styles.con}>
+        </TouchableOpacity>
+         <TouchableOpacity style={styles.con}>
             <Text style={styles.tabtext}>Tour</Text>
-        </View>
-         <View style={styles.con}>
+        </TouchableOpacity>
+         <TouchableOpacity style={styles.con}>
             <Text style={styles.tabtext}>Adventure</Text>
-        </View>
+        </TouchableOpacity>
       </View>
+      <FlatList
+        horizontal
+        data={Database}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+        contentContainerStyle={styles.listContainer}
+      />
+      <View style={{flexDirection:'row',paddingHorizontal:10,justifyContent:'space-between'}}>
+        <Text style={{fontSize:18,fontWeight:'bold',color:'#425884'}}>Popular</Text>
+        <TouchableOpacity>
+            <Text style={{fontSize:13,fontWeight:'bold',marginTop:10}}>See All</Text>
+        </TouchableOpacity>
+      </View>
+      <FlatList   
+        data={Database}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItems}
+        contentContainerStyle={styles.listContainer1}
+      />
     </SafeAreaView>
   );
 };
@@ -77,4 +117,47 @@ const styles = StyleSheet.create({
   tabtext:{
     fontWeight:'bold'
   },
+  images:{
+  width:208,
+  height:158,
+  borderRadius:20
+  },
+  itemContainer:{
+    height:250,
+    backgroundColor:'#fff',
+    padding:20,
+    borderRadius:30,
+    marginRight:10,
+    width:260
+  },
+  listContainer:{
+    padding:0,
+    marginTop:20,
+    height:250,
+    marginBottom:500
+  },
+  title:{
+    fontSize:18,
+    fontWeight:'bold',
+    marginTo:10,
+    color:'gray'
+  },
+  description:{
+    fontSize:10,
+    color:'#777'
+  },
+  listContainer1:{
+    
+  },
+  contain:{
+    flexDirection:'row',
+    height:'100%',
+    justifyContent:'space-between',
+    backgroundColor:'white',
+    padding:10,
+    borderRadius:15,
+    width:'100%',
+    padding:10
+  },
+  
 });
