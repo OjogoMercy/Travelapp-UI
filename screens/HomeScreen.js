e8847e0be340c07fe9b5b19cf7fc9c80208aeb68
Armykookie35 <mercyojogo@gmail.com>
2025-04-15 21:33:15 -0700

    update

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { SafeAreaView, StyleSheet, Text, View, StatusBar,Image } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, StatusBar,Image,TextInput, TouchableOpacity } from 'react-native';
 import React from 'react';
 import images from '@/assets/images/Images';
 import { Ionicons } from '@expo/vector-icons';
@@ -8,10 +8,26 @@ const HomeScreen = () => {
     <SafeAreaView style={styles.container}>
       <StatusBar backgroundColor={'#e4f0ff'} />
       <View style={{ flexDirection: 'row',alignItems:'center'}}>
-        <Ionicons name="arrow-back" size={30} color="#000" />
+        <TouchableOpacity>
+        <Ionicons name="arrow-back" size={22} color="#425884" style={{resizeMode:'contain',marginRight:5}} />
+        </TouchableOpacity>
         <Text style={{fontSize:18,color:'#425884'}}>Explore</Text>
-        <Image source={images.filter} style={{height:30,width:30,alignSelf:'flex-end'}}/>
+        <Image source={images.filter} style={{height:30,width:30,marginLeft:200}}/>
       </View>
+      <View style={styles.search}>
+        <TouchableOpacity>
+        <Ionicons name="search" size={20} color="#bbb" />
+        </TouchableOpacity>
+         <TextInput
+         placeholder='mount bruno'
+         placeholderTextColor={'#ccc'}
+         style={{width:'85%'}}
+         />
+         <TouchableOpacity>
+         <Ionicons name="options" size={20} color="#bbb" />
+         </TouchableOpacity>
+      </View>
+      <View style={styles.tab}></View>
     </SafeAreaView>
   );
 };
@@ -22,5 +38,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#e4f0ff',
+    padding:20
+  },
+  search:{
+    flexDirection:'row',
+    width:'100%',
+    backgroundColor:'white',
+    height:50,
+    marginTop:'30',
+    borderRadius:30,
+    alignItems:'center',
+    paddingHorizontal:10,
   },
 });
