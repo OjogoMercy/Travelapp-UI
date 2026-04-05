00bdbcdab0a174055ca378846d6974b12567a8a9
OjogoMercy <mercyojogo@gmail.com>
2026-04-05 17:22:01 +0100

    Upgraded to latest dependencies

---
+++ class.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const class = () => {
+  return (
+    <View>
+      <Text>class</Text>
+    </View>
+  )
+}
+
+export default class
+
+const styles = StyleSheet.create({})
