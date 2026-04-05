4cc412c8af3d9a60fd8406f15f9a11ce2f948eed
OjogoMercy <mercyojogo@gmail.com>
2026-04-05 18:00:09 +0100

    chore: update dependencies and add babel configuration
    - Added "react-native-worklets-core" to dependencies in package.json
    - Updated "jest-expo" version in devDependencies
    - Created babel.config.js for Babel configuration with presets and plugins

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }], // If you use NativeWind
+    ],
+    plugins: [
+      "react-native-reanimated/plugin",
+      "react-native-worklets-core/plugin",
+    ],
+  };
+};
